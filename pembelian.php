cf9256d4d868e346c49c8c5c9b97e86ecaa5abab
Rahman Yusuf <mansuf@mansuf.link>
2023-07-06 16:57:55 +0700

    asd

--- pembelian.php
+++ pembelian.php
@@ -80,7 +80,6 @@
 </script>
 
 <?php
-session_start();
 // Periksa apakah pengguna sudah login
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
   header("Location: loginapotek.php");
