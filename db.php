89fed5c9904b30ef23cb9b5b7658c67d695d6e6b
Rahman Yusuf <mansuf@mansuf.link>
2023-07-06 16:10:29 +0700

    12

--- db.php
+++ db.php
@@ -1,9 +1,19 @@
 <?php
-// deklarasi parameter koneksi database
-$server   = "localhost";
-$username = "root";
-$password = "";
-$database = "db_apotek";
+
+$DOCKER_PROCESS = getenv("DOCKER_PROCESS");
+
+if ($DOCKER_PROCESS == "true") {
+   $server = getenv("DB_HOST");
+   $username = getenv("DB_USERNAME");
+   $password = getenv("DB_PASSWORD");
+   $database = getenv("DB_SCHEME");
+} else {
+   // deklarasi parameter koneksi database
+   $server   = "localhost";
+   $username = "root";
+   $password = "";
+   $database = "db_apotek";
+}
 
 // koneksi database
 $mysqli = new mysqli($server, $username, $password, $database);
